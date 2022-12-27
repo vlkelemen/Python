ca245da8008a7de2c285884c01488640c2f75cc4
vlkelemen <115688850+vlkelemen@users.noreply.github.com>
2022-12-27 09:23:52 +0200

    Create config.py

---
+++ lab_10/config.py
@@ -0,0 +1,35 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    DEVELOPMENT = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'thesecretkey132'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
+
+
+class DevConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              'sqlite:///' + os.path.join(basedir, 'site.db')
+
+
+class TestConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_site.db')
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              'sqlite:///' + os.path.join(basedir, 'site.db')
+
+
+config = {
+    'dev': DevConfig,
+    'prod': ProdConfig,
+    'default': DevConfig,
+    'test': TestConfig,
+}
