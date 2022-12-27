41a48f9c3a4d2b581ccc7b52f832d7bec1bf7864
vlkelemen <115688850+vlkelemen@users.noreply.github.com>
2022-12-27 13:15:32 +0200

    Delete config.py

--- Lab_6/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-SECRET_KEY = 'supersecretkey'
-WTF_CSRF_ENABLED = True
-# Database
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'site.db')
