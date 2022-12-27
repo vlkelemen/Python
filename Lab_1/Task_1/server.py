8122ca30dcdf784831a268a5aeb357312c727eb9
vlkelemen <115688850+vlkelemen@users.noreply.github.com>
2022-12-27 02:50:18 +0200

    Update server.py

--- Lab_1/Task_1/server.py
+++ Lab_1/Task_1/server.py
@@ -1,23 +1,25 @@
 import socket
-from datetime import datetime
+import datetime
+import time
 
-host = 'localhost'
-port = 16789
+HOST = '127.0.0.1'
+PORT = 5000
 
-server = socket.socket()
-server.bind((host, port))
-
-server.listen(2)
-conn, addr = server.accept()
-
-print("Connection from: " + str(addr) + " at", datetime.now())
-while True:
-    # It won't accept data packet greater than 1024 bytes
-    data = conn.recv(1024).decode()
-    if not data:
-        # if data is not received - break
-        break
-    print("from connected user: " + str(data))
-    conn.send(data.encode())
-
-conn.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if data == "/stop":
+                print("jj")
+                conn.close()
+            else:
+                print(f"[{datetime.datetime.now()}] - {data}")
+                time.sleep(5)
+                try:
+                    conn.send(data)
+                    print("Message successfully sended")
+                except:
+                    print("Error")
