d8b90c233f1199ceb98b08bb3ff29182ebde3146
vlkelemen <115688850+vlkelemen@users.noreply.github.com>
2022-12-27 07:40:44 +0200

    Create __init__.py

---
+++ lab_7/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+
+from app import views, models
